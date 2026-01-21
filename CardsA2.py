640f0fae403d5c3ff222026e873c1fe650967a58
Keira <k.baker30@icloud.com>
2026-01-21 21:20:45 +0000

    New

--- CardsA2.py
+++ CardsA2.py
@@ -1,27 +1,32 @@
+import random
+
+numbers = ('A','2','3','4','5','6','7','8','9','10','J','Q','K')
+suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
+
 class Player():
     def __init__(self,name):
         self.name = name
         self.deck = None 
 
     def pickupDeck():
-        number = ('A','2','3','4','5','6','7','8','9','10','J','Q','K')
-        suit = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
-        Deck = Card(suit,number)
+        numbers = ('A','2','3','4','5','6','7','8','9','10','J','Q','K')
+        suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
+        Deck = Card(suits,numbers)  # Need to number each card (0-52) 
     def shuffleDeck():
-        pass
-    def dealCard():
-        pass
+        random(Deck)                # shuffle (random) number between 0-52 when each value is assigned a number.
+        def dealCard():
+            Deal = Deck - random(Card) and player_one or player_two + Card # do I need the 'random' or will shuffle do that for me? (Not sure how to word the 'and' statement.)
 
 class Deck():
     def __init__(self):
-        for suit in suit:
-            for number in number:
-                []
+        for suit in suits:
+            for number in numbers:
+                self.cards = (number, 'of', suit)   # Not sure?
 
 class Card():
-    def __init__(self,suit,number):
-        self.suit = suit
-        self.number = number
+    def __init__(self,suits,numbers):
+        self.suits = suits
+        self.numbers = numbers
 
 player_one = Player('Keira')
 player_two = Player('Pedro')
